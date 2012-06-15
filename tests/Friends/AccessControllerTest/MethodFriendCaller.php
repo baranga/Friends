b815b40396537c11013b8351b1ae326f90b36fb4
baranga <bar@baranga.de>
2012-06-15 10:31:15 +0200

    removed function bodies, not needed

--- tests/Friends/AccessControllerTest/MethodFriendCaller.php
+++ tests/Friends/AccessControllerTest/MethodFriendCaller.php
@@ -1,50 +1,22 @@
 <?php
 
 class Friends_AccessControllerTest_MethodFriendCaller
-    extends Friends_AccessControllerTest_AbstractCaller
 {
-    public function triggerPublicCall()
-    {
-        $this->_callee->receivePublicCall();
-    }
+    public function triggerPublicCall() {}
 
-    public function triggerProtectedCall()
-    {
-        $this->_callee->_receiveProtectedCall();
-    }
+    public function triggerProtectedCall() {}
 
-    public function triggerPrivateCall()
-    {
-        $this->_callee->_receivePrivateCall();
-    }
+    public function triggerPrivateCall() {}
 
-    public function getPublicProperty()
-    {
-        return $this->_callee->publicProperty;
-    }
+    public function getPublicProperty() {}
 
-    public function setPublicProperty($value)
-    {
-        $this->_callee->publicProperty = $value;
-    }
+    public function setPublicProperty() {}
 
-    public function getProtectedProperty()
-    {
-        return $this->_callee->_protectedProperty;
-    }
+    public function getProtectedProperty() {}
 
-    public function setProtectedProperty($value)
-    {
-        $this->_callee->_protectedProperty = $value;
-    }
+    public function setProtectedProperty() {}
 
-    public function getPrivateProperty()
-    {
-        return $this->_callee->_privateProperty;
-    }
+    public function getPrivateProperty() {}
 
-    public function setPrivateProperty($value)
-    {
-        $this->_callee->_privateProperty = $value;
-    }
+    public function setPrivateProperty() {}
 }
