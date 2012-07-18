5de17edcbe9304dc3d8c84d5774d15f54f528bc4
baranga <bar@baranga.de>
2012-07-18 10:29:39 +0200

    skip base test for all php version lower thar 5.3.2

--- tests/Friends/BaseTest.php
+++ tests/Friends/BaseTest.php
@@ -3,6 +3,14 @@
 class Friends_BaseTest
     extends PHPUnit_Framework_TestCase
 {
+    public function setUp()
+    {
+        if (version_compare(PHP_VERSION, '5.3.2', '<')) {
+            $this->markTestSkipped('PHP version < 5.3.2');
+            return;
+        }
+    }
+
     public function testDispatch()
     {
         $callee = new Friends_BaseTest_Callee();
